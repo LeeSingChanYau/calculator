623e370a6015f45e4f1a0cbcc28ca7eac5c4bb5a
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-07-12 10:14:58 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+  const [operatorClicked, setOperatorClicked] = useState(false);
+  const [currentValue, setValue] = useState(0);
+
+  const handleInput = (input: string) => {
+    if (operatorClicked) {
+      console.log('inside handleInput if');
+      setInputValue(input);
+      setOutputValue(input);
+      setOperatorClicked(false);
+    } else {
+      console.log('inside handleInput else');
+      setInputValue(inputValue + input);
+      setOutputValue(outputValue + input);
+    }
+  };
+
+  const handleSum = () => {
+    setOperatorClicked(true);
+    if (currentValue > 0) {
+      console.log('inside handleSum if');
+      let sum = currentValue + Number(inputValue);
+      setOutputValue(String(sum));
+      setInputValue('0');
+      setValue(sum);
+    } else {
+      console.log('inside handleSum else');
+      setValue(Number(inputValue));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="calculator">
+        <div className="result">{outputValue ? outputValue : '0'}</div>
+        <div className="numbers">
+          <div className="number">AC</div>
+          <div className="number">+/-</div>
+          <div className="number">%</div>
+          <div className="number">/</div>
+        </div>
+        <div className="numbers">
+          <div className="number" onClick={() => handleInput('7')}>
+            7
+          </div>
+          <div className="number" onClick={() => handleInput('8')}>
+            8
+          </div>
+          <div className="number" onClick={() => handleInput('9')}>
+            9
+          </div>
+          <div className="number">X</div>
+        </div>
+        <div className="numbers">
+          <div className="number" onClick={() => handleInput('4')}>
+            4
+          </div>
+          <div className="number" onClick={() => handleInput('5')}>
+            5
+          </div>
+          <div className="number" onClick={() => handleInput('6')}>
+            6
+          </div>
+          <div className="number">-</div>
+        </div>
+        <div className="numbers">
+          <div className="number" onClick={() => handleInput('1')}>
+            1
+          </div>
+          <div className="number" onClick={() => handleInput('2')}>
+            2
+          </div>
+          <div className="number" onClick={() => handleInput('3')}>
+            3
+          </div>
+          <div className="number" onClick={handleSum}>
+            +
+          </div>
+        </div>
+        <div className="numbers">
+          <div className="zero">0</div>
+          <div className="number">.</div>
+          <div className="number">=</div>
+        </div>
+      </div>
     </div>
   );
 }
